9f67e7f7ee34368fa8cdb656a693d942857b82c0
Kris <zenosumo@gmail.com>
2025-09-06 00:34:53 +0200

    standardization and formatting

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import {heroui} from '@heroui/theme';
+import type {Config} from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'dark-gradient': 'linear-gradient(to top, rgba(0,0,0,0.8), transparent)',
+      },
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)'],
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [heroui()],
+};
+
+export default config;
