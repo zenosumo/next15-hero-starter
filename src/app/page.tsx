7ab0f96a0bcc0a0a1410a44ab5e9817b56265ff9
Kris <zenosumo@gmail.com>
2025-09-06 01:18:11 +0200

    text formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,36 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
+import {Code} from '@heroui/code';
+import {Link} from '@heroui/link';
+import {Snippet} from '@heroui/snippet';
+import {button as buttonStyles} from '@heroui/theme';
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import {GithubIcon} from '@/components/icons';
+import {subtitle, title} from '@/components/primitives';
+import {siteConfig} from '@/config/site';
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
         <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <span className={title({color: 'violet'})}>beautiful&nbsp;</span>
         <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
+        <span className={title()}>websites regardless of your design experience.</span>
+        <div className={subtitle({class: 'mt-4'})}>Beautiful, fast and modern React UI library.</div>
       </div>
 
       <div className="flex gap-3">
         <Link
           isExternal
           className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
+            color: 'primary',
+            radius: 'full',
+            variant: 'shadow',
           })}
           href={siteConfig.links.docs}
         >
           Documentation
         </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
+        <Link isExternal className={buttonStyles({variant: 'bordered', radius: 'full'})} href={siteConfig.links.github}>
           <GithubIcon size={20} />
           GitHub
         </Link>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,26 +1,24 @@
-"use client";
+'use client';
 
-import type { ThemeProviderProps } from "next-themes";
+import type {ThemeProviderProps} from 'next-themes';
 
-import * as React from "react";
-import { HeroUIProvider } from "@heroui/system";
-import { useRouter } from "next/navigation";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import {HeroUIProvider} from '@heroui/system';
+import {ThemeProvider as NextThemesProvider} from 'next-themes';
+import {useRouter} from 'next/navigation';
+import * as React from 'react';
 
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
 }
 
-declare module "@react-types/shared" {
+declare module '@react-types/shared' {
   interface RouterConfig {
-    routerOptions: NonNullable<
-      Parameters<ReturnType<typeof useRouter>["push"]>[1]
-    >;
+    routerOptions: NonNullable<Parameters<ReturnType<typeof useRouter>['push']>[1]>;
   }
 }
 
-export function Providers({ children, themeProps }: ProvidersProps) {
+export function Providers({children, themeProps}: ProvidersProps) {
   const router = useRouter();
 
   return (
