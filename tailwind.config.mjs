58fc3479a058e12486fe536225a1f607b153f00b
Kris <zenosumo@gmail.com>
2025-09-06 15:46:41 +0200

    nexjs confgiguration optimization

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 import {heroui} from '@heroui/theme';
-import type {Config} from 'tailwindcss';
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
