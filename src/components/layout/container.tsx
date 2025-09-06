72ca92e1fd1087cfa14643e7675c1baff6799601
Kris <zenosumo@gmail.com>
2025-09-06 13:45:27 +0200

    layout and settings

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -7,11 +7,11 @@ type ContainerProps = {
 
 export default function Container({children, className}: ContainerProps) {
   return (
-    <body>
+    <>
       {/* 
         Origianl class name: flex flex-col flex-grow h-full pb-6 py-0 pt-0 items-center justify-center
       */}
       <div className={cn('', className)}>{children}</div>
-    </body>
+    </>
   );
 }
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: 'Next.js + HeroUI',
-  description: 'Make beautiful websites regardless of your design experience.',
+  name: 'Starter',
+  description: 'Make beautiful websites...',
   navItems: [
     {
       label: 'Home',
